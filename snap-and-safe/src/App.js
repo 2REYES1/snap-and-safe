f55d7f79597abdd46be053880ec94dfe2334d721
IleanaEscobar <izescobar5@gmail.com>
2024-04-20 12:21:17 -0700

    add terniary to switch component on success

--- snap-and-safe/src/App.js
+++ snap-and-safe/src/App.js
@@ -49,7 +49,7 @@ function App() {
             <Route
               path="/pillSuccess"
               element={(
-                <PillSuccess/>
+                <PillSuccess response={"blah blah blah"}/>
               )}
             />
           </Routes>
