fd0882b9972a5db57386c3c7e9a7775ef7c68c70
Alyssa Reyes <alyssalareyes@gmail.com>
2024-04-20 00:25:24 -0700

    did the layout for the uploading-image part and added some css

--- snap-and-safe/src/Components/FoodCheck/FoodCheck.jsx
+++ snap-and-safe/src/Components/FoodCheck/FoodCheck.jsx
@@ -1,8 +1,51 @@
 import './FoodCheck.css';
 import * as React from 'react';
+import { useState, useRef } from 'react';
 
 export default function FoodCheck() {
+
+    const [selectedFile, setSelectedFile] = useState(null);
+    const fileInputRef = useRef(null);
+
+    const handleFileChange = (e) => {
+        setSelectedFile(e.target.files[0]);
+    };
+
+    const handleButtonClick = () => {
+        fileInputRef.current.click();
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(selectedFile);
+    };
+
     return (
-        <div>food check</div>
+        <div class = "main-layout">
+            <div class = "header-layout">
+                <button class = "prev-button">
+                    Prev
+                </button>
+            </div>
+            <div class = "box-center">
+                <div class = "upload-box box-center">
+                    <p>upload an image of your food label.</p>
+                    <form onSubmit={handleSubmit}>
+                    <input
+                    type="file"
+                    ref={fileInputRef}
+                    style={{ display: 'none' }}
+                    onChange={handleFileChange}
+                />
+                <button type="button" onClick={handleButtonClick}>
+                    Select a File
+                </button>
+                <button type="submit">Upload</button>
+            </form>
+                </div>
+            </div>
+        </div>
     );
 }
+
+
