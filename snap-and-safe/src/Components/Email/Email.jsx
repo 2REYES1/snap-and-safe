a99b6d9a45f77b04cd2083fa2691849e6bd16a91
Ernest Yao <nightmoon0731@gmail.com>
2024-04-19 23:18:55 -0700

    email.jsx added

---
+++ snap-and-safe/src/Components/Email/Email.jsx
@@ -0,0 +1,10 @@
+import './Email.css';
+import * as React from 'react';
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router';
+
+export default function Email() {
+    return (
+        <div>hi email</div>
+    );
+}
